00e7a0824916b7a17194c493969a86a88e2e6eee
tigattack <10629864+tigattack@users.noreply.github.com>
2023-08-18 15:42:07 +0100

    initial edits for fork

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from setuptools import setup
 
-setup(name='td4a',
-      version='2.0.3',
-      description='A browser based jinja template renderer',
-      url='http://github.com/cidrblock/td4a',
-      author='Bradley A. Thornton',
-      author_email='brad@thethorntons.net',
-      license='MIT',
-      include_package_data=True,
-      packages=[
-          'td4a'
-      ],
-      scripts=['td4a-server'],
-      install_requires=[
-          'ansible==2.9.12',
-          'Flask==1.1.2',
-          'netaddr==0.8.0',
-          'Twisted==20.3.0',
-          'requests==2.24.0',
-          'ruamel.yaml==0.16.10',
-          'genson==1.2.1',
-          'jsonschema==3.2.0'
-      ],
-      zip_safe=False)
+setup(
+    name='td4a',
+    version='2.0.3',
+    description='A browser based jinja template renderer',
+    url='http://github.com/tigattack/td4a',
+    author='Bradley A. Thornton (fork by tigattack)',
+    license='MIT',
+    include_package_data=True,
+    packages=['td4a'],
+    scripts=['td4a-server'],
+    install_requires=[
+        'ansible==2.9.12',
+        'Flask==1.1.2',
+        'netaddr==0.8.0',
+        'Twisted==20.3.0',
+        'requests==2.24.0',
+        'ruamel.yaml==0.16.10',
+        'genson==1.2.1',
+        'jsonschema==3.2.0'
+    ],
+    zip_safe=False
+)
