341a72bf491b2451bd64a8308e1382e0d35f27da
tigattack <10629864+tigattack@users.noreply.github.com>
2023-08-22 01:23:53 +0100

    get install requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+# gross.
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    required = f.read().splitlines()
+
 setup(
     name='td4a',
     version='2.0.3',
@@ -10,15 +14,6 @@ setup(
     include_package_data=True,
     packages=['src/td4a'],
     scripts=['src/td4a-server'],
-    install_requires=[
-        'ansible==2.9.12',
-        'Flask==1.1.2',
-        'netaddr==0.8.0',
-        'Twisted==20.3.0',
-        'requests==2.24.0',
-        'ruamel.yaml==0.16.10',
-        'genson==1.2.1',
-        'jsonschema==3.2.0'
-    ],
-    zip_safe=False
+    install_requires=required,
+    zip_safe=False,
 )
